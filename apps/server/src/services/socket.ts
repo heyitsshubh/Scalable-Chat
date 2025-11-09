db68996432487b297ea6fbfe60d1dcfc842d61e2
unknown <shubhgupta4102@gmail.com>
2025-11-09 21:32:36 +0530

    redis initiation

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,15 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub= new Redis({
+    host: "127.0.0.1",
+  port: 6379
+
+});
+const sub= new Redis({
+   host: "127.0.0.1",
+  port: 6379
+});
 
 class SocketService {
     private _io: Server | null = null;
@@ -6,9 +17,11 @@ class SocketService {
       console.log("SocketService initialized");
       this._io = new Server({
         cors: {
+        allowedHeaders: ["*"],
           origin: "*",
         },
       });
+         sub.subscribe("messages");
     }
 
     public initlisteners() {
@@ -20,11 +33,23 @@ class SocketService {
         }
         io.on("connection", (socket) => {
             console.log(`New client connected: ${socket.id}`);
-            socket.on("event:message", (data) => {
+            socket.on("event:message", async (data) => {
                 console.log(`Message received: ${data}`);
+               await pub.publish("messages", JSON.stringify({ message: data }));
+
             });
         });
+
+        sub.on("message", (channel, message) => {
+            if (channel === "messages") {
+                const parsedMessage = JSON.parse(message);
+                console.log(`Broadcasting message: ${parsedMessage.message}`);
+                io.emit("event:message", parsedMessage.message);
+            }
+        });
     }
+
+        
 
     get io(): Server | null {
         return this._io;
--- apps/web/context/Socketcontext.tsx
+++ apps/web/context/Socketcontext.tsx
@@ -1,21 +1,49 @@
 "use client"
 import React from "react";
+import { io,Socket } from "socket.io-client";
 
 interface SocketProviderProps {
     children?:React.ReactNode;
 }
 
 interface IsocketContext{
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     sendMessage:(message:string)=>any;
 }
 
 const SocketContext = React.createContext<IsocketContext['sendMessage'] | null>(null);
 
-export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+export const useSocket = () => {
+    const context = React.useContext(SocketContext);
+    if (!context) {
+        throw new Error("useSocket must be used within a SocketProvider");
+    }
+    return context;
+}
 
+export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+    const [socket, setSocket] = React.useState<Socket | null>(null);
     const sendMessage:IsocketContext['sendMessage']= React.useCallback((message:string)=>{
         console.log("Sending message:", message);
+        if(socket){
+            socket.emit("event:message", message);
+        }
+    },[socket]);
+
+  const onmessageReceived=React.useCallback((message:string)=>{
+        console.log("Message received from server:", message);
     },[]);
+    
+    React.useEffect(() => {
+        const socket = io("http://localhost:3000");
+        socket.on("event:message", onmessageReceived);
+        setSocket(socket);
+        return () => {
+            socket.disconnect();
+            socket.off("event:message", onmessageReceived);
+            setSocket(null);
+        };
+    }, []);
     return(
         <SocketContext.Provider value={sendMessage}>
             {children}
